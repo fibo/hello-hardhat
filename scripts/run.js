95e3ed903a79696f23b2abbe6e3cee383b6cdcd7
fibo <casati_gianluca@yahoo.it>
2022-03-20 13:34:14 +0100

    run first contract with hardhat

---
+++ scripts/run.js
@@ -0,0 +1,19 @@
+const main = async () => {
+  // hre stands for Hardhat Runtime Environment
+  const nftContractFactory = await hre.ethers.getContractFactory("MyEpicNFT");
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
+  console.log("Contract deployed to:", nftContract.address);
+};
+
+const run = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+run();
